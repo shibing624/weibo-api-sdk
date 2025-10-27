8ecd8e609c49b16bc5025587d88e58f42ae85063
shibing624 <shibing624@126.com>
2025-10-27 22:21:52 +0800

    version.

--- weibo_api_sdk/__init__.py
+++ weibo_api_sdk/__init__.py
@@ -4,7 +4,7 @@ Weibopy - 微博 API Python 客户端
 一个免登陆获取新浪微博数据的Python库，简单易用。
 """
 
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 __author__ = 'xuming'
 __email__ = 'xuming624@qq.com'
 __license__ = 'MIT'
